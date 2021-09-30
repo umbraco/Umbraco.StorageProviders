e91cd0a65634f7cecabf34a817c6c5ccc11b9048
Ronald Barendse <ronald@barend.se>
2021-09-30 11:28:11 +0200

    Add initial IStorageProvider abstraction

---
+++ src/Umbraco.StorageProviders/Abstractions/IStorageProvider.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+
+namespace Umbraco.StorageProviders
+{
+    /// <summary>
+    /// A read/write storage provider abstraction.
+    /// </summary>
+    /// <seealso cref="Microsoft.Extensions.FileProviders.IFileProvider" />
+    public interface IStorageProvider : IFileProvider
+    {
+        /// <summary>
+        /// Create a writable stream to store file contents. Caller should dispose the stream when complete.
+        /// </summary>
+        /// <param name="subpath">The path/name of the file.</param>
+        /// <param name="overwrite"><c>true</c> if the file can be overwritten; otherwise, <c>false</c>.</param>
+        /// <returns>
+        /// The file stream.
+        /// </returns>
+        Stream? CreateWriteStream(string subpath, bool overwrite = false);
+
+        /// <summary>
+        /// Moves the specified file to a new location, providing the option to specify a new file name.
+        /// </summary>
+        /// <param name="sourceSubpath">The path/name of the source file.</param>
+        /// <param name="destinationSubpath">The path/name of the destination file. This cannot be a directory.</param>
+        /// <param name="overwrite"><c>true</c> if the destination file can be overwritten; otherwise, <c>false</c>.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// Returns a value indicating whether the operation succeeded.
+        /// </returns>
+        Task<bool> MoveAsync(string sourceSubpath, string destinationSubpath, bool overwrite = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Moves the specified directory to a new location.
+        /// </summary>
+        /// <param name="sourceSubpath">The path/name of the source directory.</param>
+        /// <param name="destinationSubpath">The path/name of the destination directory.</param>
+        /// <param name="overwrite"><c>true</c> if files in the destination directory can be overwritten; otherwise, <c>false</c>.</param>
+        /// <param name="recursive"><c>true</c> to recursively move subdirectories and files; otherwise, <c>false</c>.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// Returns a value indicating whether the operation succeeded.
+        /// </returns>
+        Task<bool> MoveDirectoryAsync(string sourceSubpath, string destinationSubpath, bool overwrite = false, bool recursive = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Copies an existing file to a new file, providing the option to specify a new file name.
+        /// </summary>
+        /// <param name="sourceSubpath">The path/name of the source directory.</param>
+        /// <param name="destinationSubpath">The path/name of the destination file. This cannot be a directory.</param>
+        /// <param name="overwrite"><c>true</c> if the destination file can be overwritten; otherwise, <c>false</c>.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// Returns a value indicating whether the operation succeeded.
+        /// </returns>
+        Task<bool> CopyAsync(string sourceSubpath, string destinationSubpath, bool overwrite = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Copies an existing directory to a new directory.
+        /// </summary>
+        /// <param name="sourceSubpath">The path/name of the source directory.</param>
+        /// <param name="destinationSubpath">The path/name of the destination directory.</param>
+        /// <param name="overwrite"><c>true</c> if files in the destination directory can be overwritten; otherwise, <c>false</c>.</param>
+        /// <param name="recursive"><c>true</c> to recursively copy subdirectories and files; otherwise, <c>false</c>.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// Returns a value indicating whether the operation succeeded.
+        /// </returns>
+        Task<bool> CopyDirectoryAsync(string sourceSubpath, string destinationSubpath, bool overwrite = false, bool recursive = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes the specified file.
+        /// </summary>
+        /// <param name="subpath">The path/name of the file.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// Returns a value indicating whether the operation succeeded.
+        /// </returns>
+        Task<bool> DeleteAsync(string subpath, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes the specified directory.
+        /// </summary>
+        /// <param name="subpath">The path/name of the directory.</param>
+        /// <param name="recursive"><c>true</c> to recursively remove subdirectories and files; otherwise, <c>false</c>.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// Returns a value indicating whether the operation succeeded.
+        /// </returns>
+        Task<bool> DeleteDirectoryAsync(string subpath, bool recursive = false, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Umbraco.StorageProviders/FileSystem/FileSystemFileProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using Umbraco.Cms.Core.IO;
+using Umbraco.StorageProviders.FileSystem;
+
+namespace Umbraco.StorageProviders
+{
+    /// <summary>
+    /// Exposes an <see cref="IFileSystem" /> as an <see cref="IFileProvider" />.
+    /// </summary>
+    /// <seealso cref="Microsoft.Extensions.FileProviders.IFileProvider" />
+    public class FileSystemFileProvider : IFileProvider
+    {
+        /// <summary>
+        /// The file system.
+        /// </summary>
+        protected IFileSystem FileSystem { get; }
+
+        /// <summary>
+        /// The path prefix.
+        /// </summary>
+        protected string? PathPrefix { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileSystemFileProvider" /> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="pathPrefix">The path prefix.</param>
+        /// <exception cref="System.ArgumentNullException">fileSystem</exception>
+        public FileSystemFileProvider(IFileSystem fileSystem, string? pathPrefix = null)
+        {
+            FileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
+            PathPrefix = pathPrefix;
+        }
+
+        /// <inheritdoc />
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            var path = PathPrefix + subpath;
+            if (path == null || !FileSystem.DirectoryExists(path))
+            {
+                return NotFoundDirectoryContents.Singleton;
+            }
+
+            return new FileSystemDirectoryContents(FileSystem, path);
+        }
+
+        /// <inheritdoc />
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            var path = PathPrefix + subpath;
+            if (path == null || !FileSystem.FileExists(path))
+            {
+                return new NotFoundFileInfo(path);
+            }
+
+            return new FileSystemFileInfo(FileSystem, path);
+        }
+
+        /// <inheritdoc />
+        public IChangeToken Watch(string filter) => NullChangeToken.Singleton;
+    }
+}
