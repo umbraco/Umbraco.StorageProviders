01bb12f98f977b79064e9141fb8c6724848b704f
Ronald Barendse <ronald@barend.se>
2022-04-13 23:09:49 +0200

    Ensure RemoveMediaFromPath also removes customized media path

--- src/Umbraco.StorageProviders/CdnMediaUrlProvider.cs
+++ src/Umbraco.StorageProviders/CdnMediaUrlProvider.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Hosting;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Core.Routing;
@@ -10,48 +12,66 @@ namespace Umbraco.StorageProviders
     /// A <see cref="IMediaUrlProvider" /> that returns a CDN URL for a media item.
     /// </summary>
     /// <seealso cref="Umbraco.Cms.Core.Routing.DefaultMediaUrlProvider" />
-    public class CdnMediaUrlProvider : DefaultMediaUrlProvider
+    public sealed class CdnMediaUrlProvider : DefaultMediaUrlProvider
     {
-        private bool _removeMediaFromPath;
         private Uri _cdnUrl;
+        private bool _removeMediaFromPath;
+        private string _mediaPath;
 
         /// <summary>
         /// Creates a new instance of <see cref="CdnMediaUrlProvider" />.
         /// </summary>
         /// <param name="options">The options.</param>
+        /// <param name="globalSettings">The global settings.</param>
+        /// <param name="hostingEnvironment">The hosting environment.</param>
         /// <param name="mediaPathGenerators">The media path generators.</param>
         /// <param name="uriUtility">The URI utility.</param>
         /// <exception cref="System.ArgumentNullException">options</exception>
-        public CdnMediaUrlProvider(IOptionsMonitor<CdnMediaUrlProviderOptions> options, MediaUrlGeneratorCollection mediaPathGenerators, UriUtility uriUtility)
+        public CdnMediaUrlProvider(IOptionsMonitor<CdnMediaUrlProviderOptions> options, IOptionsMonitor<GlobalSettings> globalSettings, IHostingEnvironment hostingEnvironment, MediaUrlGeneratorCollection mediaPathGenerators, UriUtility uriUtility)
             : base(mediaPathGenerators, uriUtility)
         {
-            if (options == null) throw new ArgumentNullException(nameof(options));
+            ArgumentNullException.ThrowIfNull(options);
+            ArgumentNullException.ThrowIfNull(globalSettings);
+            ArgumentNullException.ThrowIfNull(hostingEnvironment);
 
             _cdnUrl = options.CurrentValue.Url;
             _removeMediaFromPath = options.CurrentValue.RemoveMediaFromPath;
+            _mediaPath = hostingEnvironment.ToAbsolute(globalSettings.CurrentValue.UmbracoMediaPath).TrimEnd('/');
 
-            options.OnChange(OptionsOnChange);
+            options.OnChange((options, name) =>
+            {
+                if (name == Options.DefaultName)
+                {
+                    _removeMediaFromPath = options.RemoveMediaFromPath;
+                    _cdnUrl = options.Url;
+                }
+            });
+
+            globalSettings.OnChange((options, name) =>
+            {
+                if (name == Options.DefaultName)
+                {
+                    _mediaPath = hostingEnvironment.ToAbsolute(options.UmbracoMediaPath).TrimEnd('/');
+                }
+            });
         }
 
         /// <inheritdoc />
         public override UrlInfo? GetMediaUrl(IPublishedContent content, string propertyAlias, UrlMode mode, string culture, Uri current)
         {
             var mediaUrl = base.GetMediaUrl(content, propertyAlias, UrlMode.Relative, culture, current);
-            if (mediaUrl == null) return null;
-
-            return mediaUrl.IsUrl switch
+            if (mediaUrl?.IsUrl == true)
             {
-                false => mediaUrl,
-                _ => UrlInfo.Url($"{_cdnUrl}/{mediaUrl.Text[(_removeMediaFromPath ? "/media/" : "/").Length..]}", culture)
-            };
-        }
+                string url = mediaUrl.Text;
+                if (_removeMediaFromPath && url.StartsWith(_mediaPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    url = url[_mediaPath.Length..];
+                }
 
-        private void OptionsOnChange(CdnMediaUrlProviderOptions options, string name)
-        {
-            if (name != Options.DefaultName) return;
+                return UrlInfo.Url(_cdnUrl + url, culture);
+            }
 
-            _removeMediaFromPath = options.RemoveMediaFromPath;
-            _cdnUrl = options.Url;
+            return mediaUrl;
         }
     }
 }
