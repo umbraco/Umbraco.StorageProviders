425c7b9a520c9737f1a617cbc17b3cdfff83b07c
Ronald Barendse <ronald@barend.se>
2022-10-19 11:30:47 +0200

    Allow changing AzureBlobFileSystemImageCache container root path

--- src/Umbraco.StorageProviders.AzureBlob.ImageSharp/AzureBlobFileSystemImageCache.cs
+++ src/Umbraco.StorageProviders.AzureBlob.ImageSharp/AzureBlobFileSystemImageCache.cs
@@ -1,9 +1,11 @@
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.Extensions.Options;
 using SixLabors.ImageSharp.Web;
 using SixLabors.ImageSharp.Web.Caching;
 using SixLabors.ImageSharp.Web.Resolvers;
 using SixLabors.ImageSharp.Web.Resolvers.Azure;
+using Umbraco.Extensions;
 using Umbraco.StorageProviders.AzureBlob.IO;
 
 namespace Umbraco.StorageProviders.AzureBlob.ImageSharp;
@@ -13,29 +15,20 @@ namespace Umbraco.StorageProviders.AzureBlob.ImageSharp;
 /// </summary>
 public sealed class AzureBlobFileSystemImageCache : IImageCache
 {
-    private const string CachePath = "cache/";
+    private readonly string? _containerRootPath;
     private BlobContainerClient _container;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AzureBlobFileSystemImageCache" /> class.
     /// </summary>
     /// <param name="options">The options.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="options" /> is <c>null</c>.</exception>
-    public AzureBlobFileSystemImageCache(IOptionsMonitor<AzureBlobFileSystemOptions> options)
-        : this(AzureBlobFileSystemOptions.MediaFileSystemName, options)
-    { }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AzureBlobFileSystemImageCache"/> class.
-    /// </summary>
     /// <param name="name">The name.</param>
-    /// <param name="options">The options.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="name" /> is <c>null</c>.</exception>
+    /// <param name="containerRootPath">The container root path.</param>
     /// <exception cref="ArgumentNullException"><paramref name="options" /> is <c>null</c>.</exception>
-    public AzureBlobFileSystemImageCache(string name, IOptionsMonitor<AzureBlobFileSystemOptions> options)
+    public AzureBlobFileSystemImageCache(IOptionsMonitor<AzureBlobFileSystemOptions> options, string name, string? containerRootPath)
     {
-        ArgumentNullException.ThrowIfNull(name);
         ArgumentNullException.ThrowIfNull(options);
+        ArgumentNullException.ThrowIfNull(name);
 
         var fileSystemOptions = options.Get(name);
         _container = new BlobContainerClient(fileSystemOptions.ConnectionString, fileSystemOptions.ContainerName);
@@ -47,20 +40,33 @@ public sealed class AzureBlobFileSystemImageCache : IImageCache
                 _container = new BlobContainerClient(options.ConnectionString, options.ContainerName);
             }
         });
+
+        if (!string.IsNullOrEmpty(containerRootPath))
+        {
+            _containerRootPath = containerRootPath.EnsureEndsWith('/');
+        }
     }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AzureBlobFileSystemImageCache" /> class.
     /// </summary>
     /// <param name="blobContainerClient">The blob container client.</param>
+    /// <param name="containerRootPath">The container root path.</param>
     /// <exception cref="ArgumentNullException"><paramref name="blobContainerClient" /> is <c>null</c>.</exception>
-    public AzureBlobFileSystemImageCache(BlobContainerClient blobContainerClient)
-        => _container = blobContainerClient ?? throw new ArgumentNullException(nameof(blobContainerClient));
+    public AzureBlobFileSystemImageCache(BlobContainerClient blobContainerClient, string? containerRootPath)
+    {
+        _container = blobContainerClient ?? throw new ArgumentNullException(nameof(blobContainerClient));
+
+        if (!string.IsNullOrEmpty(containerRootPath))
+        {
+            _containerRootPath = containerRootPath.EnsureEndsWith('/');
+        }
+    }
 
     /// <inheritdoc />
     public async Task<IImageCacheResolver?> GetAsync(string key)
     {
-        var blob = _container.GetBlobClient(CachePath + key);
+        var blob = _container.GetBlobClient(_containerRootPath + key);
 
         return !await blob.ExistsAsync().ConfigureAwait(false)
             ? null
@@ -70,8 +76,11 @@ public sealed class AzureBlobFileSystemImageCache : IImageCache
     /// <inheritdoc />
     public async Task SetAsync(string key, Stream stream, ImageCacheMetadata metadata)
     {
-        var blob = _container.GetBlobClient(CachePath + key);
+        var blob = _container.GetBlobClient(_containerRootPath + key);
 
-        await blob.UploadAsync(stream, metadata: metadata.ToDictionary()).ConfigureAwait(false);
+        await blob.UploadAsync(stream, new BlobUploadOptions()
+        {
+            Metadata = metadata.ToDictionary()
+        }).ConfigureAwait(false);
     }
 }
--- src/Umbraco.StorageProviders.AzureBlob.ImageSharp/DependencyInjection/AddAzureBlobImageSharpCacheExtensions.cs
+++ src/Umbraco.StorageProviders.AzureBlob.ImageSharp/DependencyInjection/AddAzureBlobImageSharpCacheExtensions.cs
@@ -8,36 +8,51 @@ using Umbraco.StorageProviders.AzureBlob.IO;
 namespace Umbraco.Cms.Core.DependencyInjection;
 
 /// <summary>
-/// Extension methods to help registering Azure Blob Storage image caches for ImageSharp.
+/// Extension methods to help register an Azure Blob Storage image cache for ImageSharp.
 /// </summary>
 public static class AddAzureBlobImageSharpCacheExtensions
 {
-    /// <summary>
-    /// Registers an <see cref="IImageCache" /> configured using the specified <see cref="IAzureBlobFileSystem" />.
-    /// </summary>
-    /// <param name="builder">The <see cref="IUmbracoBuilder" />.</param>
-    /// <param name="name">The name of the file system.</param>
-    /// <returns>
-    /// The <see cref="IUmbracoBuilder" />.
-    /// </returns>
-    /// <exception cref="System.ArgumentNullException"><paramref name="builder" /> is <c>null</c>.</exception>
-    public static IUmbracoBuilder AddAzureBlobImageSharpCache(this IUmbracoBuilder builder, string name = AzureBlobFileSystemOptions.MediaFileSystemName)
-        => builder.AddInternal(name);
+    private const string ContainerRootPath = "cache";
 
     /// <summary>
-    /// Registers an <see cref="IImageCache" /> configured using the specified <see cref="IAzureBlobFileSystem" />.
+    /// Registers an <see cref="IImageCache" /> configured using the <see cref="AzureBlobFileSystemOptions" /> for media and <see cref="ContainerRootPath" /> container root path.
     /// </summary>
     /// <param name="builder">The <see cref="IUmbracoBuilder" />.</param>
-    /// <param name="name">The name of the file system.</param>
     /// <returns>
     /// The <see cref="IUmbracoBuilder" />.
     /// </returns>
     /// <exception cref="System.ArgumentNullException"><paramref name="builder" /> is <c>null</c>.</exception>
-    internal static IUmbracoBuilder AddInternal(this IUmbracoBuilder builder, string name = AzureBlobFileSystemOptions.MediaFileSystemName)
+    public static IUmbracoBuilder AddAzureBlobImageSharpCache(this IUmbracoBuilder builder)
+        => builder.AddInternal(AzureBlobFileSystemOptions.MediaFileSystemName, ContainerRootPath);
+
+    /// <summary>
+    /// Registers an <see cref="IImageCache" /> configured using the specified <see cref="AzureBlobFileSystemOptions" />.
+    /// </summary>
+    /// <param name="builder">The <see cref="IUmbracoBuilder" />.</param>
+    /// <param name="name">The name of the file system.</param>
+    /// <param name="containerRootPath">The container root path.</param>
+    /// <returns>
+    /// The <see cref="IUmbracoBuilder" />.
+    /// </returns>
+    /// <exception cref="System.ArgumentNullException"><paramref name="builder" /> is <c>null</c>.</exception>
+    public static IUmbracoBuilder AddAzureBlobImageSharpCache(this IUmbracoBuilder builder, string name, string? containerRootPath = null)
+        => builder.AddInternal(name, containerRootPath);
+
+    /// <summary>
+    /// Registers an <see cref="IImageCache" /> configured using the specified <see cref="AzureBlobFileSystemOptions" />.
+    /// </summary>
+    /// <param name="builder">The <see cref="IUmbracoBuilder" />.</param>
+    /// <param name="name">The name of the file system.</param>
+    /// <param name="containerRootPath">The container root path.</param>
+    /// <returns>
+    /// The <see cref="IUmbracoBuilder" />.
+    /// </returns>
+    /// <exception cref="System.ArgumentNullException"><paramref name="builder" /> is <c>null</c>.</exception>
+    internal static IUmbracoBuilder AddInternal(this IUmbracoBuilder builder, string name, string? containerRootPath)
     {
         ArgumentNullException.ThrowIfNull(builder);
 
-        builder.Services.AddUnique<IImageCache>(provider => new AzureBlobFileSystemImageCache(name, provider.GetRequiredService<IOptionsMonitor<AzureBlobFileSystemOptions>>()));
+        builder.Services.AddUnique<IImageCache>(provider => new AzureBlobFileSystemImageCache(provider.GetRequiredService<IOptionsMonitor<AzureBlobFileSystemOptions>>(), name, containerRootPath));
 
         return builder;
     }
