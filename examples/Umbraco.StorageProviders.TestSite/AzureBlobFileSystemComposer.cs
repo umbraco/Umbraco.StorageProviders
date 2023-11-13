cc6f0171257f2838e2ce3c1165ef2eb9c4c1d2ee
Ronald Barendse <ronald@barend.se>
2023-11-13 16:18:01 +0100

    Add example test site

---
+++ examples/Umbraco.StorageProviders.TestSite/AzureBlobFileSystemComposer.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.StorageProviders.AzureBlob.IO;
+
+namespace Umbraco.StorageProviders.TestSite;
+
+internal sealed class AzureBlobFileSystemComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+        => builder
+        .AddAzureBlobMediaFileSystem()
+        .AddAzureBlobImageSharpCache()
+        .AddNotificationHandler<UnattendedInstallNotification, AzureBlobMediaFileSystemCreateIfNotExistsHandler>();
+
+    private sealed class AzureBlobMediaFileSystemCreateIfNotExistsHandler : INotificationHandler<UnattendedInstallNotification>
+    {
+        private readonly AzureBlobFileSystemOptions _options;
+
+        public AzureBlobMediaFileSystemCreateIfNotExistsHandler(IOptionsMonitor<AzureBlobFileSystemOptions> options)
+            => _options = options.Get(AzureBlobFileSystemOptions.MediaFileSystemName);
+
+        public void Handle(UnattendedInstallNotification notification)
+            => AzureBlobFileSystem.CreateIfNotExists(_options);
+    }
+}
