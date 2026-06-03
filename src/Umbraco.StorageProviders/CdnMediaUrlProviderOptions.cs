4772c0bc019e3a747fd8b3dcd84dc454fb4932a7
Ronald Barendse <ronald@barend.se>
2026-06-03 17:36:28 +0200

    Use a private const for the CdnMediaUrlProviderOptions default value

--- src/Umbraco.StorageProviders/CdnMediaUrlProviderOptions.cs
+++ src/Umbraco.StorageProviders/CdnMediaUrlProviderOptions.cs
@@ -8,6 +8,8 @@ namespace Umbraco.StorageProviders;
 /// </summary>
 public sealed class CdnMediaUrlProviderOptions
 {
+    private const bool DefaultRemoveMediaFromPath = true;
+
     /// <summary>
     /// Gets or sets the CDN media root URL.
     /// </summary>
@@ -23,6 +25,6 @@ public sealed class CdnMediaUrlProviderOptions
     /// <value>
     ///   <c>true</c> if the media path needs to be removed from the path; otherwise, <c>false</c>.
     /// </value>
-    [DefaultValue(true)]
-    public bool RemoveMediaFromPath { get; set; } = true;
+    [DefaultValue(DefaultRemoveMediaFromPath)]
+    public bool RemoveMediaFromPath { get; set; } = DefaultRemoveMediaFromPath;
 }
