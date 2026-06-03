103f5015720bd578e0da5e093ecce2bb4782c078
Ronald Barendse <ronald@barend.se>
2026-06-04 01:21:12 +0200

    Update dependencies, fix GetBlobsByHierarchy and audit only direct dependencies

--- src/Umbraco.StorageProviders.AzureBlob/AzureBlobFileProvider.cs
+++ src/Umbraco.StorageProviders.AzureBlob/AzureBlobFileProvider.cs
@@ -97,7 +97,7 @@ public sealed class AzureBlobFileProvider : IFileProvider
         var path = GetFullPath(subpath);
 
         // Get all blobs and iterate to fetch all pages
-        var blobs = _containerClient.GetBlobsByHierarchy(delimiter: "/", prefix: path).ToList();
+        var blobs = _containerClient.GetBlobsByHierarchy(BlobTraits.None, BlobStates.None, delimiter: "/", prefix: path).ToList();
 
         return blobs.Count == 0
             ? NotFoundDirectoryContents.Singleton
--- src/Umbraco.StorageProviders.AzureBlob/IO/AzureBlobFileSystem.cs
+++ src/Umbraco.StorageProviders.AzureBlob/IO/AzureBlobFileSystem.cs
@@ -479,7 +479,7 @@ public sealed class AzureBlobFileSystem : IAzureBlobFileSystem, IFileProviderFac
         string? delimiter = recursive ? null : "/";
         string prefix = GetFullPath(path).EnsureEndsWith('/');
 
-        return _container.GetBlobsByHierarchy(delimiter: delimiter, prefix: prefix);
+        return _container.GetBlobsByHierarchy(BlobTraits.None, BlobStates.None, delimiter: delimiter, prefix: prefix);
     }
 
     private string GetBlobName(string path)
